5c5636dbd132501ac306e0d004c7347783fc3461
sbraende <sebastian.braende@gmail.com>
2024-05-24 15:06:39 +0200

    Added highlight leader

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let homeScore = 0 
 let guestScore = 0 
 
+let homeBgEl = document.getElementById("home-bg")
+let guestBgEl = document.getElementById("guest-bg")
+
 function updateScore(team, score) {
     let teamEl = document.getElementById(team)
     
@@ -12,4 +15,29 @@ function updateScore(team, score) {
         guestScore += score
         teamEl.textContent = guestScore    
     }
+
+    highlightLeader() 
 }
+
+function highlightLeader() {
+    if (homeScore == guestScore) {
+        homeBgEl.style.border = "none"
+        guestBgEl.style.border = "none"
+    } else if (homeScore > guestScore) {
+        homeBgEl.style.border = "4px solid #F94F6D"
+        guestBgEl.style.border = "none"
+    } else if (homeScore < guestScore) {
+        homeBgEl.style.border = "none"
+        guestBgEl.style.border = "4px solid #F94F6D"
+    }
+}
+
+function reset() {
+    homeScore = 0
+    guestScore = 0
+    document.getElementById("home").textContent = 0
+    document.getElementById("guest").textContent = 0
+    homeBgEl.style.border = "none"
+    guestBgEl.style.border = "none"
+}
+
