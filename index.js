0d3237f2f2fd33095f81f7e01c9d040787bf2159
sbraende <sebastian.braende@gmail.com>
2024-05-24 14:16:13 +0200

    Update score function added

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
-
-let home = document.getElementById("home")
-let guest = document.getElementById("guest")
-
 let homeScore = 0 
 let guestScore = 0 
 
-function homeAddOne() {
-    homeScore += 1
-    home.textContent = homeScore
-}
-
-function homeAddTwo() {
-    homeScore += 2
-    home.textContent = homeScore
-}
-
-function homeAddThree() {
-    homeScore += 3
-    home.textContent = homeScore
-}
-
-
-function guestAddOne() {
-    guestScore += 1
-    guest.textContent = guestScore
-}
-
-function guestAddTwo() {
-    guestScore += 2
-    guest.textContent = guestScore
-}
-
-function guestAddThree() {
-    guestScore += 3
-    guest.textContent = guestScore
+function updateScore(team, score) {
+    let teamEl = document.getElementById(team)
+    
+    if (team == "home") {
+        homeScore += score
+        teamEl.textContent = homeScore
+    }
+    else {
+        guestScore += score
+        teamEl.textContent = guestScore    
+    }
 }
