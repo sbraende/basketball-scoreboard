cfdc332c6363f76c80b545fff9f74eb4bbe5f64c
sbraende <sebastian.braende@gmail.com>
2024-05-27 11:11:53 +0200

    Optimized code

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let guestScore = 0
 let homeBgEl = document.getElementById("home-bg")
 let guestBgEl = document.getElementById("guest-bg")
 
+let clrRed = "4px solid #F94F6D"
+let clrBg = "4px solid #1B244A"
+
 function updateScore(team, score) {
     let teamEl = document.getElementById(team)
     
@@ -21,14 +24,14 @@ function updateScore(team, score) {
 
 function highlightLeader() {
     if (homeScore == guestScore) {
-        homeBgEl.style.border = "none"
-        guestBgEl.style.border = "none"
+        homeBgEl.style.border = clrRed
+        guestBgEl.style.border = clrRed
     } else if (homeScore > guestScore) {
-        homeBgEl.style.border = "4px solid #F94F6D"
-        guestBgEl.style.border = "none"
+        homeBgEl.style.border = clrRed
+        guestBgEl.style.border = clrBg
     } else if (homeScore < guestScore) {
-        homeBgEl.style.border = "none"
-        guestBgEl.style.border = "4px solid #F94F6D"
+        homeBgEl.style.border = clrBg
+        guestBgEl.style.border = clrRed
     }
 }
 
@@ -37,7 +40,7 @@ function reset() {
     guestScore = 0
     document.getElementById("home").textContent = 0
     document.getElementById("guest").textContent = 0
-    homeBgEl.style.border = "none"
-    guestBgEl.style.border = "none"
+    homeBgEl.style.border = clrBg
+    guestBgEl.style.border = clrBg
 }
 
